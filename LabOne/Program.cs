bb64e83682747e4c8138a3943a08b905ef7db9e8
coutaq <coutaq@gmail.com>
2019-09-27 15:57:05 +0300

    роаа

--- LabOne/Program.cs
+++ LabOne/Program.cs
@@ -6,7 +6,24 @@ namespace LabOne
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x = 0;
+            Console.Write("Please enter your number: ");
+            bool tryAgain = true;
+            while (tryAgain)
+            {
+                try
+                {
+                    x = double.Parse(Console.ReadLine());
+                    tryAgain = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Incorrect input!");
+                }
+            }
+            int d = Double ((x%1)* 10);
+            Console.WriteLine(d);
         }
+
     }
 }
